26fcddca63dd37b2455acec00a3965db616e8fd1
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-08-18 23:20:38 -0400

    setup

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿var numbers = new List<int> { 5, 10, 15, 20 };
+var calculator = new NumbersSumCalculator();
+
+Console.WriteLine(calculator.Calculate(numbers));
+Console.ReadKey();
+
+public class NumbersSumCalculator
+{
+    public int Calculate(List<int> nums)
+    {
+        int sum = 0;
+
+        foreach(var num in nums)
+        {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
